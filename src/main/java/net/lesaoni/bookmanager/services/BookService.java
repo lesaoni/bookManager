bb24f7c2c93c6f36de7ef0c7c6102ea61b06f6d4
lesaoni <lesaoni@yandex.ru>
2019-06-13 13:51:48 +0300

    --BookManager Initial Commit

---
+++ src/main/java/net/lesaoni/bookmanager/services/BookService.java
@@ -0,0 +1,15 @@
+package net.lesaoni.bookmanager.services;
+
+import net.lesaoni.bookmanager.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    void addBook(Book book);
+    void updateBook(Book book);
+    void removeBook(int id);
+    Book getBookById(int id);
+    List<Book> listBooks();
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Lesaoni</title>
+</head>
+<body>
+<h3>Lesaoni presents</h3>
+<br/>
+<a href="/books" target="_blank">Book List</a>
+</body>
+</html>
